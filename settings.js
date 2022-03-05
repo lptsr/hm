fc3cdc82a37d2c10f63d9e70e12e234661bd1f9a
lptsr <92776179+lptsr@users.noreply.github.com>
2022-03-05 11:37:28 +0700

    update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "𝑳𝒖𝒏𝒂𝒊𝒓 𝑩𝒐𝒕𝒛-Md" //namabot kalian
+global.ownername= "𝐿𝑒𝓃𝓃𝒾𝒸𝒶𝓃𝓈︶ε╰✿" //nama kalian
 global.myweb ="https://api-alphabot.herokuapp.com/" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.youtube = "https://instagram.com/iam._lptsr" //bebas asal jan hapus
+global.github = "https://github.com/lptsr" //bebas
+global.email = "lunairbotz@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.ownernomer = "6281554828172" // nomor wa kalian
+global.ownernomerr = "+6281554828172" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
-global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.donasi = "./image/lol.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["62887435047326","62887435047326","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.owner = ["6281554828172","6281554828172","62857830372011"] //ganti agar fitur owner bisa di gunakan
+global.packname = '©𝑳𝒖𝒏𝒂𝒊𝒓 𝑩𝒐𝒕𝒛-Md' //sticker wm ubah
+global.author = 'ᵇʸ ˡᵉⁿⁿⁱᶜᵃⁿˢ' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
